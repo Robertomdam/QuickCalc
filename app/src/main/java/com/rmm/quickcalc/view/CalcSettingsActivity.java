507b2c29dedf21588c252cca1957e55e65d0c2f7
Roberto <robertomdam@gmail.com>
2020-07-20 17:51:09 +0200

    Initial commit

---
+++ app/src/main/java/com/rmm/quickcalc/view/CalcSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.rmm.quickcalc.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rmm.quickcalc.R;
+
+public class CalcSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_settings);
+    }
+}
